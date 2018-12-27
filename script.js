64e7b23fc211ef0fa6acd7cba126b20503a7d98e
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2018-12-27 14:24:39 +0100

    resizing done

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const square = document.createElement("div")
 document.body.appendChild(square)
+let grow = true
 let size = 100
 square.style.height = size + "px"
 square.style.width = size + "px"
 window.addEventListener("scroll", () => {
-    size++
-    square.style.width = size + "px"
-    square.style.height = size + "px"
-
+    if(size >= window.innerWidth /2 ) {
+        grow = !grow
+    }
+    else if (size <= 0  ) {
+        grow = !grow
+    }
+    if (grow) {
+        size +=5
+        square.style.width = size + "px"
+        square.style.height = size + "px"
+    }
+    else {
+        size -=5
+        square.style.width = size + "px"
+        square.style.height = size + "px"
+    }
+  
 })
